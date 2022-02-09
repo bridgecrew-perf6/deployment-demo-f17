0242ea51c92ce0de60f4c1a8f3704ce63e5ccf04
Wesley Lo <wesleylo1995@gmail.com>
2022-02-09 11:58:22 -0600

    added configuration for heroku

--- server/server.js
+++ server/server.js
@@ -2,16 +2,20 @@
 
 const express = require('express')
 const cors = require('cors')
+const path = require('path') // build into node, needed to import bigger files
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 
+app.get('/',(req,res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))//__dirname (auto populate from heroku) is directory name. joining directory name with index.html in this case
+})
 
+// heroku might not have 4000 or whatever port we choose
+const port = process.env.PORT || 4000
 
-
-const server = 4000
-app.listen(server, () => {
+app.listen(port, () => {
     console.log('server running on port 4000')
 })
