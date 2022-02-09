4cfc2f9479cdf683177eba0a1a12899cac85710f
Wesley Lo <wesleylo1995@gmail.com>
2022-02-09 12:16:52 -0600

    port

--- server/server.js
+++ server/server.js
@@ -18,5 +18,5 @@ app.get('/',(req,res) => {
 const port = process.env.PORT || 4000
 
 app.listen(port, () => {
-    console.log('server running on port 4000')
+    console.log(`server running on port ${port}`)
 })
